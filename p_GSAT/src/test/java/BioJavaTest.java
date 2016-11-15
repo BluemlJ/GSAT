4ccd14cd1faa9e43997c7d5215e1e1ef8becd572
Lovis Heindrich <lovisheindrich@yahoo.de>
2016-11-15 16:08:35 +0100

    3rd try

--- p_GSAT/src/test/java/BioJavaTest.java
+++ p_GSAT/src/test/java/BioJavaTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class BioJavaTest {
 
     @Test
-    public void testIntegration() {
+    public void testIntegration() throws CompoundNotFoundException{
 	DNASequence seq = new DNASequence("GTAC"); 
 	assertTrue("seq should be initialized", (seq instanceof DNASequence));
     }
