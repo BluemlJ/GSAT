572bc0e83e6baabd7f07ba09ed7d2f08d4496ddf
Lovis Heindrich <lovisheindrich@yahoo.de>
2016-11-17 11:27:53 +0100

    next try to integrate biojava

--- p_GSAT/src/test/java/test/BioJavaTest.java
+++ p_GSAT/src/test/java/test/BioJavaTest.java
@@ -1,6 +1,7 @@
 package test;
 import static org.junit.Assert.*;
 import org.biojava.nbio.core.sequence.*;
+import org.biojava.nbio.core.exceptions.*;
 import org.junit.Test;
 
 public class BioJavaTest {
@@ -8,11 +9,11 @@ public class BioJavaTest {
     // DOES NOT WORK, probably because the BioJava framework is not globally integrated
     // into the project's resources.
     
-    /*
+    
     @Test
     public void testIntegration() throws CompoundNotFoundException{
 	DNASequence seq = new DNASequence("GTAC"); 
 	assertTrue("seq should be initialized", (seq instanceof DNASequence));
-    }*/
+    }
 
 }
