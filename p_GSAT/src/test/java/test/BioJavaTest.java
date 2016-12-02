8e2b4d34bf4af9077a7c848ef0503290a517fab2
Lovis <lovisheindrich@yahoo.de>
2016-12-02 10:49:56 +0100

    adjusted reading tests to public ab1 files

--- p_GSAT/src/test/java/test/BioJavaTest.java
+++ p_GSAT/src/test/java/test/BioJavaTest.java
@@ -2,11 +2,8 @@ package test;
 
 import static org.junit.Assert.assertTrue;
 
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
-import javax.imageio.ImageIO;
 
 import org.biojava.bio.BioError;
 import org.biojava.bio.program.abi.ABITrace;
@@ -15,109 +12,83 @@ import org.biojava.bio.symbol.IllegalAlphabetException;
 import org.biojava.bio.symbol.IllegalSymbolException;
 import org.jcvi.jillion.trace.chromat.Chromatogram;
 import org.jcvi.jillion.trace.chromat.ChromatogramFactory;
-//import org.biojava.nbio.core.exceptions.CompoundNotFoundException;
-//import org.biojava.nbio.core.sequence.DNASequence;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
- * Tests to check the correct integration of the BioJava framework in our
- * project.
+ * Tests to check the correct integration of the BioJava framework in our project.
  * 
  * @author Lovis Heindrich
  *
  */
 public class BioJavaTest {
+  private File myFile;
+  private ABITrace myTrace;
+  private Chromatogram croma;
 
-	/**
-	 * This test checks whether the BioJavaFramework 4.2 is usable (i.e. it is
-	 * configured correctly) by referring to classes from the framework.
-	 * 
-	 * @throws CompoundNotFoundException
-	 * 
-	 * @author Lovis Heindrich
-	 */
-	// @Test
-	// public void testIntegration() throws CompoundNotFoundException {
-	// DNASequence seq = new DNASequence("GTAC");
-	// assertTrue(seq.getLength() == 4);
-	// }
+  @Before
+  public void initializeABIData() throws IOException {
+    myFile = new File(
+        getClass().getClassLoader().getResource("ab1/Tk_Gs40Hits/Forward/95EI60.ab1").getFile());
+    myTrace = new ABITrace(myFile);
+    croma = ChromatogramFactory.create(myFile);
+  }
 
-	/**
-	 * This Test checks whether the BioJava Legacy Framework is usable
-	 * 
-	 * @throws IOException
-	 * 
-	 * @author Lovis Heindrich
-	 * @throws IllegalSymbolException
-	 * @throws BioError
-	 * @throws IllegalAlphabetException
-	 */
-	@Test
-	public void testABIRead() throws IOException, IllegalSymbolException, IllegalAlphabetException, BioError {
-		// placeholder - real test needs local files
-		assertTrue(true);
-		File myFile = new
-		// File("D:/Dokumente/Dropbox/BP_GSAT/Materialien/Dateien/Bsp/AB/93GH02_A01.ab1");
-		File("/home/bluemlj/Dropbox/BP_GSAT/Materialien/Dateien/Bsp/AB/93GH02_A04.ab1");
-		// assertTrue("File access worked", myFile.canRead());
-		ABITrace myTrace = new ABITrace(myFile);
-		// ABIFParser myParser = new ABIFParser(myFile);
-		// ABIFChromatogram myChroma = new ABIFChromatogram();
+  /**
+   * This Test checks whether the BioJava Legacy Framework is usable
+   * 
+   * @throws IOException
+   * 
+   * @author Lovis Heindrich
+   * @throws IllegalSymbolException
+   * @throws BioError
+   * @throws IllegalAlphabetException
+   */
+  @Test
+  public void testABIRead()
+      throws IOException, IllegalSymbolException, IllegalAlphabetException, BioError {
+    // scale has to be 2
+    /*
+     * BufferedImage chromaImg = myTrace.getImage(1000, 2); File outputfile = new
+     * File("/home/bluemlj/Desktop/chroma.png"); ImageIO.write(chromaImg, "png", outputfile);
+     */
+    int[] aTrace = myTrace.getTrace(DNATools.a());
+    int[] cTrace = myTrace.getTrace(DNATools.c());
+    int[] gTrace = myTrace.getTrace(DNATools.g());
+    int[] tTrace = myTrace.getTrace(DNATools.t());
+    int[] basecalls = myTrace.getBasecalls();
 
-		// The map has no particular order and so cannot be relied on to iterate
-		// over records in the
-		// same order they were read from the file.
-		/*
-		 * Map<String,ABIFParser.TaggedDataRecord> dataMap =
-		 * myParser.getAllDataRecords(); for
-		 * (Map.Entry<String,ABIFParser.TaggedDataRecord> entry :
-		 * dataMap.entrySet()) { System.out.println(entry.getKey() + ", " +
-		 * entry.getValue()); }
-		 */
+    for (int i = 0; i < basecalls.length; i++) {
+      // System.out.println(basecalls[i]);
+    }
+    System.out.println(tTrace.length);
+    System.out.println(basecalls[basecalls.length - 1]);
+    System.out.println((double) basecalls[basecalls.length - 1] / (double) tTrace.length);
 
-		// scale has to be 2
-		BufferedImage chromaImg = myTrace.getImage(1000, 2);
-		File outputfile = new File("/home/bluemlj/Desktop/chroma.png");
-		ImageIO.write(chromaImg, "png", outputfile);
-		int[] aTrace = myTrace.getTrace(DNATools.a());
-		int[] cTrace = myTrace.getTrace(DNATools.c());
-		int[] gTrace = myTrace.getTrace(DNATools.g());
-		int[] tTrace = myTrace.getTrace(DNATools.t());
-		int[] basecalls = myTrace.getBasecalls();
+    // System.out.println(myTrace.getSequenceLength());
+    // SymbolList mySequence = myTrace.getSequence();
+    // assertEquals(mySequence.subStr(1, 6), "gagttt");
+    // System.out.println(mySequence.seqString());
+  }
 
-		for (int i = 0; i < basecalls.length; i++) {
-			// System.out.println(basecalls[i]);
-		}
-		System.out.println(tTrace.length);
-		System.out.println(basecalls[basecalls.length - 1]);
-		System.out.println((double) basecalls[basecalls.length - 1] / (double) tTrace.length);
+  /**
+   * This test checks, if the nucleotidestring from BioJava and Jillion are identical
+   * 
+   * @throws IOExceptionif the file is not fount
+   * 
+   * @author bluemlj
+   */
+  @Test
+  public void testABIReadJillionBioJava() throws IOException {
 
-		// System.out.println(myTrace.getSequenceLength());
-		// SymbolList mySequence = myTrace.getSequence();
-		// assertEquals(mySequence.subStr(1, 6), "gagttt");
-		// System.out.println(mySequence.seqString());
-	}
+    String testString1 = myTrace.getSequence().seqString();
+    String testString2 = croma.getNucleotideSequence().toString();
 
-	/**
-	 * This test checks, if the nucleotidestring from BioJava and Jillion are
-	 * identical
-	 * 
-	 * @throws IOExceptionif
-	 *             the file is not fount
-	 * 
-	 * @author bluemlj
-	 */
-	@Test
-	public void testABIReadJillionBioJava() throws IOException {
-		File myFile = new File("/home/bluemlj/Dropbox/BP_GSAT/Materialien/Dateien/Bsp/AB/93GH02_A04.ab1");
-		ABITrace myTrace = new ABITrace(myFile);
-		Chromatogram croma = ChromatogramFactory.create(myFile);
-		String testString1 = myTrace.getSequence().seqString();
-		String testString2 = croma.getNucleotideSequence().toString();
+    testString1 = testString1.toLowerCase();
+    testString2 = testString2.toLowerCase();
 
-		testString1 = testString1.toLowerCase();
-		testString2 = testString2.toLowerCase();
+    assertTrue(testString1.equals(testString2));
+  }
 
-		assertTrue(testString1.equals(testString2));
-	}
+  
 }
