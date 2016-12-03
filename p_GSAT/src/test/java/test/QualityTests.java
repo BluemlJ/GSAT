2a09aa11e6a85133613b6fbefb641f89eabacfbb
Lovis <lovisheindrich@yahoo.de>
2016-12-03 22:56:17 +0100

    trim test

--- p_GSAT/src/test/java/test/QualityTests.java
+++ p_GSAT/src/test/java/test/QualityTests.java
@@ -9,6 +9,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import analysis.AnalysedSequence;
+import analysis.QualityAnalysis;
 import exceptions.FileReadingException;
 import io.SequenceReader;
 
@@ -34,5 +35,14 @@ public class QualityTests {
     assertEquals((int) testSequence.getAvgQuality(), 36);
     // test if the quality array is accessible
     assertEquals(testSequence.getQuality()[0], 16);
+  }
+  
+  /**
+   * Tests if the quality trim function is usable
+   * @throws IOException 
+   */
+  @Test
+  public void testQualityTrim() throws IOException{
+    assertEquals(QualityAnalysis.findLowQualityClippingPosition(testSequence), 0);
   }
 }
