3c6f446be82d8633eadb450fe692ae13810f801f
Lovis <lovisheindrich@yahoo.de>
2016-12-05 11:32:03 +0100

    next try

--- p_GSAT/src/test/java/test/BioJavaJillionTest.java
+++ p_GSAT/src/test/java/test/BioJavaJillionTest.java
@@ -2,6 +2,8 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+
 import org.biojava.bio.seq.DNATools;
 import org.jcvi.jillion.experimental.primer.PrimerUtil;
 import org.junit.Test;
@@ -16,6 +18,12 @@ public class BioJavaJillionTest {
   @Test
   public void testJillionAccessibility(){
     assertEquals(PrimerUtil.M13_FORWARD_PRIMER.toString(), "TGTAAAACGACGGCCAGT");
+  }
+  
+  @Test
+  public void testFileAccessibility(){
+    File testFile = new File(getClass().getResource("/ab1/Tk_Gs40Hits/Forward/95EI60.ab1").getFile());
+    assertTrue(testFile.exists());
   }
 }
 
