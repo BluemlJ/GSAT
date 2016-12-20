f7b0d3aaaf026d0b0538d51d49aaea10512669d0
Lovis Heindrich <lovisheindrich@yahoo.de>
2016-12-20 16:09:38 +0100

    gene reader

--- p_GSAT/src/main/java/io/GeneReader.java
+++ p_GSAT/src/main/java/io/GeneReader.java
@@ -1,24 +1,65 @@
 package io;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 
+/**
+ * This class reads reference genes from a txt and stores them
+ * @author lovisheindrich
+ *
+ */
 public class GeneReader {
   private static String path;
-
+  private static HashMap<String, String> geneList;
   
-  public static void readGenes(String path) throws IOException{
-    
-    BufferedReader geneReader = new BufferedReader(new FileReader(path));
+  /**
+   * reads a gene.txt from a given path
+   * @param genePath
+   * @throws IOException
+   */
+  public static void readGenes(String genePath) throws IOException {
+
+    BufferedReader geneReader = new BufferedReader(new FileReader(genePath));
     String line;
-    while((line = geneReader.readLine()) != null){
+    // for each line
+    while ((line = geneReader.readLine()) != null) {
+      // format "name=atgAAT..."
       String sepLine[] = line.split("=");
-      System.out.println(sepLine[0]);
-      System.out.println(sepLine[1]);
+      String name = sepLine[0];
+      String gene = sepLine[1];
+      geneList.put(name, gene);
     }
-      
+
     geneReader.close();
   }
+  
+  /**
+   * reads genes from gene file at the locally stored path
+   * @throws IOException
+   */
+  public static void readGenes() throws IOException {
+    readGenes(path);
+  }
+  
+  /**
+   * set the path of the gene.txt file
+   * @param path
+   */
+  public static void setPath(String path) {
+    GeneReader.path = path;
+  }
+  
+  /**
+   * 
+   * @param geneName
+   * @return
+   */
+  public static String getGene(String geneName){
+    return geneList.get(geneName);
+  }
+  
+
 }
+
