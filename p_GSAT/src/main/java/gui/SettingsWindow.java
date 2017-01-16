79d477a5043faa93a98287efd0893dba05c78e70
Kevin Otto <Kevin Otto>
2017-01-16 18:35:05 +0100

    added all variables to settings Window

--- p_GSAT/src/main/java/gui/SettingsWindow.java
+++ p_GSAT/src/main/java/gui/SettingsWindow.java
@@ -5,12 +5,43 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class SettingsWindow  extends Application implements javafx.fxml.Initializable{
+
+  //buttons
+  @FXML
+  private Button returnButton;  
+  @FXML
+  private Button restoreButton;
+  @FXML
+  private Button addResearcherButton;
+  @FXML
+  private Button deleteResearcherButton;
+  @FXML
+  private Button addGeneButton;
+  @FXML
+  private Button deleteGeneButton;
+  @FXML
+  private Button editGeneButton;
+  
+  //fields
+  @FXML
+  private TextField Parameter1Field;
+  
+  @FXML
+  private ChoiceBox<String> researcherDrobdown;
+  
+  @FXML
+  private ListView<String> geneList;
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
