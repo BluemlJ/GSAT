351c897856f2bed2b696fbb5ca45c714a4c105bc
Lovis Heindrich <lovisheindrich@yahoo.de>
2017-03-01 12:08:44 +0100

    database window test

--- p_GSAT/src/main/java/gui/DatabaseSettingsWindow.java
+++ p_GSAT/src/main/java/gui/DatabaseSettingsWindow.java
@@ -4,7 +4,11 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import io.ConfigHandler;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -37,7 +41,55 @@ public class DatabaseSettingsWindow extends Application implements javafx.fxml.I
 
   @Override
   public void initialize(URL arg0, ResourceBundle arg1) {
+	  GUIUtils.setColorOnButton(connectButton, ButtonColor.GREEN);
+	  GUIUtils.setColorOnButton(closeButton, ButtonColor.RED);
+	  
+	  userNameField.textProperty().addListener(new ChangeListener<String>() {
+	      @Override
+	      public void changed(ObservableValue<? extends String> observable, String oldValue,
+	          String newValue) {
+	        if (newValue.matches(ConfigHandler.SEPARATOR_CHAR + "")) {
+	          userNameField.setText(oldValue);
+	        } else {
+	          userNameField.setText(newValue);
+	        }
+	      }
+	    });
 
+	  portField.textProperty().addListener(new ChangeListener<String>() {
+	      @Override
+	      public void changed(ObservableValue<? extends String> observable, String oldValue,
+	          String newValue) {
+	        if (! newValue.matches("[0-9]+")) {
+	        	portField.setText(oldValue);
+	        } else {
+	        	portField.setText(newValue);
+	        }
+	      }
+	    });
+	  
+	  adressField.textProperty().addListener(new ChangeListener<String>() {
+	      @Override
+	      public void changed(ObservableValue<? extends String> observable, String oldValue,
+	          String newValue) {
+	        if (newValue.matches(ConfigHandler.SEPARATOR_CHAR + "")) {
+	        	adressField.setText(oldValue);
+	        } else {
+	        	adressField.setText(newValue);
+	        }
+	      }
+	    });
+	  
+	  
+	  closeButton.setOnAction(new EventHandler<ActionEvent>() {
+
+	      @Override
+	      public void handle(ActionEvent arg0) {
+	        Stage stage = (Stage) closeButton.getScene().getWindow();
+	        stage.close();
+
+	      }
+	    });
   }
 
   @Override
