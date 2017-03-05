7a4ea95d867e4d94f75ec2499d7cd4d05132064b
Lovis Heindrich <lovisheindrich@yahoo.de>
2017-03-05 12:19:14 +0100

    biojava integration... again...

---
+++ p_GSAT/src/test/java/test/BioJavaTest.java
@@ -0,0 +1,83 @@
+package test;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.biojava.bio.BioError;
+import org.biojava.bio.program.abi.ABITrace;
+import org.biojava.bio.seq.DNATools;
+import org.biojava.bio.symbol.IllegalAlphabetException;
+import org.biojava.bio.symbol.IllegalSymbolException;
+import org.jcvi.jillion.trace.chromat.Chromatogram;
+import org.jcvi.jillion.trace.chromat.ChromatogramFactory;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests to check the correct integration of the BioJava framework in our
+ * project.
+ * 
+ * @author Lovis Heindrich
+ *
+ */
+public class BioJavaTest {
+	private File myFile;
+	private ABITrace myTrace;
+	private Chromatogram croma;
+
+	@Before
+	public void initializeABIData() throws IOException {
+		myFile = new File("resources/ab1/Tk_Gs40Hits/Forward/95EI60.ab1");
+		myTrace = new ABITrace(myFile);
+		croma = ChromatogramFactory.create(myFile);
+	}
+
+	/**
+	 * This Test checks whether the BioJava Legacy Framework is usable
+	 * 
+	 * @throws IOException
+	 * 
+	 * @author Lovis Heindrich
+	 * @throws IllegalSymbolException
+	 * @throws BioError
+	 * @throws IllegalAlphabetException
+	 */
+	@Test
+	public void testABIRead() throws IOException, IllegalSymbolException, IllegalAlphabetException, BioError {
+		// scale has to be 2
+		/*
+		 * BufferedImage chromaImg = myTrace.getImage(1000, 2); File outputfile
+		 * = new File("/home/bluemlj/Desktop/chroma.png");
+		 * ImageIO.write(chromaImg, "png", outputfile);
+		 */
+		int[] aTrace = myTrace.getTrace(DNATools.a());
+		int[] cTrace = myTrace.getTrace(DNATools.c());
+		int[] gTrace = myTrace.getTrace(DNATools.g());
+		int[] tTrace = myTrace.getTrace(DNATools.t());
+		int[] basecalls = myTrace.getBasecalls();
+	}
+
+	/**
+	 * This test checks, if the nucleotidestring from BioJava and Jillion are
+	 * identical
+	 * 
+	 * @throws IOExceptionif
+	 *             the file is not fount
+	 * 
+	 * @author bluemlj
+	 */
+	@Test
+	public void testABIReadJillionBioJava() throws IOException {
+
+		String testString1 = myTrace.getSequence().seqString();
+		String testString2 = croma.getNucleotideSequence().toString();
+
+		testString1 = testString1.toLowerCase();
+		testString2 = testString2.toLowerCase();
+
+		assertTrue(testString1.equals(testString2));
+	}
+
+}
